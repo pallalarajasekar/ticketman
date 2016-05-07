3d833e77a643b4801cf3d43fcda7ff45f69ffc59
pallalarajasekar <mepc1217@gmail.com>
2016-05-07 19:31:34 +0530

    Add crypto code

--- web_server.js
+++ web_server.js
@@ -13,15 +13,27 @@ var config = {
     socketIO: true
 }
 
+//var us = require('underscore');
+var assert = require('assert');
 var XPressIO = require('xpressio');
 var xpress = new XPressIO(config).start();
 var app = xpress.app;
 var io = xpress.io;
 
+/*POC Modules*/
+var node_cryptojs = require('node-cryptojs-aes');
+var CryptoJS = node_cryptojs.CryptoJS;
+var crypto = require('crypto');
+var randomstring = require('randomstring');
+
+
 var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://localhost:27017/database';
+var db = null
+MongoClient.connect("mongodb://localhost:27017/database", function(err, database) {
+    db = database;
+    assert.equal(null, err);
+
+});
 
 
 app.get('/', function(req, res){
@@ -30,50 +42,80 @@ app.get('/', function(req, res){
 
 app.post('/find', function(req,res){})
 
+app.post("/register",function(req,res){
+
+    console.log(req.body);
+
+    /*var collection=db.collection('user');
+    var doc1={"username":req.body.Username,"password":req.body.Password,"mobileno":req.body.Phoneno,"email":req.body.Email};
+    collection.insert(doc1,{w:1},function(err,result){
+        console.log(err,result);
+        res.send(result);
+    })*/
+    setTimeout(function() {
+        var pass = req.body.Password;
+        var salt = generateSalt();
+        var collection = db.collection('user');
+        var doc1 = {
+            "username": req.body.Username,
+            "password": salt + md5(pass + salt),
+            "mobileno": req.body.Phoneno,
+            "email": req.body.Email
+        };
+        collection.insert(doc1, {w: 1}, function (err, result) {
+            console.log(err, result);
+            res.send(result);
+            console.log("SALT : ", salt);
+            console.log("DIGESTED_PWD : ", md5(pass + salt))
+            console.log("Encrypted Password : ", salt + md5(pass + salt))
+        }, 3000);
+    })
+})
+
+
+var generateSalt = function()
+{
+    var set = '0123456789abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ';
+    var salt = '';
+    for (var i = 0; i < 10; i++) {
+        var p = Math.floor(Math.random() * set.length);
+        salt += set[p];
+    }
+    return salt;
+};
+
+
+var md5 = function(str) {
+    return crypto.createHash('md5').update(str).digest('hex');
+};
+
+var saltAndHash = function(pass, callback)
+{
+    var salt = generateSalt();
+    callback(null, salt + md5(pass + salt));
+};
+
+
+
+
+/*setTimeout(function(){
+    var pass = req.body.Password;
+    var salt = generateSalt();
+    console.log("SALT : ", salt);
+    console.log("DIGESTED_PWD : ", md5(pass + salt))
+    console.log("Encrypted Password : ",  salt +md5(pass + salt))
+}, 3000);*/
+
+
+
 app.post("/loginform",function(req,res){
 
-    //console.log(req.body);
+    console.log(req.body);
+    console.log(req.sessionID);
 
-    /*data={
-        "uname": req.body.Username,
-        "pwd" : req.body.Password
-    }
-    console.log(data.pwd);*/
-
-    var logindata = function(db, callback) {
-        var cursor =db.collection('user').findOne( function(err, doc) {
-            console.log(doc);
-
-                if(doc.name==req.body.Username && doc.password==req.body.Password){
-
-                    console.log("successfully login")
-                    res.send(req.body);
-                }
-                else{
-                    console.log("Invalid");
-                    res.send(req.body);
-                }
-        });
-           /* } else {
-                callback();
-            }
-            /!*console.log("name:" +doc.name);
-            console.log("PWD:" +doc.password);
-*!/
-
-        });*/
-        /*console.log("name:" +doc.name);
-        console.log("PWD:" +doc.password);*/
-    };
-    MongoClient.connect(url, function(err, db) {
-        assert.equal(null, err);
-        logindata(db, function() {
-            //db.close();
-        });
-    });
-
-
-
+    req.session.destroy(function(err){
+        //console.log(err,req.sessionID)
+    })
 
 })
 
